abb34bbf7661440bc93885cab6ed16511cd337ef
FedExPipah <55308518+FedExPipah@users.noreply.github.com>
2019-10-07 12:27:41 -0700

    Update angle-lines.js

--- angle-lines.js
+++ angle-lines.js
@@ -1,12 +1,13 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(650, 600);
   colorMode(HSB);
 }
 
 function draw() {
   background(255);
 
-  for (var x = 10; x < width - 10; x = x + 10) {
-    line(x, height/2, x+100, height/2-75);
+  for (var x = 100; x < width - 10; x = x + 10) {
+    line(x, height/2,x+100,height/2-75);
+stroke(random(220),40,180);
   }
 }
