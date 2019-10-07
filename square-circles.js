8ec45929e856597aa5be88115128630219abf89c
FedExPipah <55308518+FedExPipah@users.noreply.github.com>
2019-10-07 12:48:19 -0700

    Update square-circles.js

--- square-circles.js
+++ square-circles.js
@@ -13,12 +13,12 @@ function draw() {
   x.push(mouseX);
   y.push(mouseY);
 
-  rectMode(CENTER);
+rectMode(CENTER);
 
-  for (var i = 0; i < x.length; i = i + 10) {
+  for (var i = 5; i < x.length; i = i + 3) {
 
-    rect(x[i], x[i], 1 + (x.length - i), 1 + (y.length - i));
-    stroke(x[i], y[i], 100);
+    rect(x[i], x[i], 1 + (x.length - i), 1 + (x.length - i));
+    stroke(x[i], y[i], 100,40);
 
   }
   x = x.slice(-50); // remove all but the last 50 x values
