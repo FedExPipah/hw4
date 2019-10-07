5f512a72bb42085ca580b7c9f82e2d51c5bb09e1
FedExPipah <55308518+FedExPipah@users.noreply.github.com>
2019-10-07 12:19:15 -0700

    Update colored-lines.js

--- colored-lines.js
+++ colored-lines.js
@@ -1,14 +1,15 @@
 function setup() {
   createCanvas(600, 650);
+   colorMode(HSB);
 }
 function draw() {
   background(255);
-  colorMode(HSB);
-  stroke(random(310), 15, 78);
-   fill(random(255), 100, 100);
 
-  for (var x = 10; x < width + 50; x = x + 20) {
+  
+  for (var x = 10; x < width -10; x = x + 10) {
     line(x, height / 1, mouseX, mouseY);
+    stroke(x,mouseY, mouseX);
+
   }
 
 }
